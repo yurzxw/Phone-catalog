d0d3c5b67673b570c0631d780d65874260f3a0c3
Yura Dziavoruk <yurzxw@MacBook-Air-Yura.local>
2025-08-10 17:58:22 +0300

    Nice Gadgets

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
 import './App.scss';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import { HomePage } from './modules/Home page/HomePage';
+import { ProductPage } from './modules/Product page/ProductPage';
+import { ProductDetailsPage } from './modules/Product details page/ProductDetailsPage';
+import { CartProvider } from './context/CartContext';
+import { CartPage } from './modules/Cart/Cart';
+import { FavouritesPage } from './modules/Favourites page/FavouritesPage';
+import { FavouritesProvider } from './context/FavouritesContext';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import './i18n';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <FavouritesProvider>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route
+                path="/phones"
+                element={<ProductPage category="phones" />}
+              />
+              <Route
+                path="/tablets"
+                element={<ProductPage category="tablets" />}
+              />
+              <Route
+                path="/accessories"
+                element={<ProductPage category="accessories" />}
+              />
+              <Route
+                path="/:category/:productId"
+                element={<ProductDetailsPage />}
+              />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/favourites" element={<FavouritesPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </CartProvider>
+    </FavouritesProvider>
   );
 };
